9e3623f79e55b1c22f1fba883e7f5347ea5e6318
DELL <DELL@DESKTOP-FJ2D18H>
2022-01-20 18:01:47 +0530

    second Push

---
+++ com.crm.comcast.sdet28/src/test/java/OrgnaizationTest/DeleteOrganization.java
@@ -0,0 +1,14 @@
+package OrgnaizationTest;
+
+import org.testng.annotations.Test;
+
+import com.crm.comcast.GenericUtility.BaseClass;
+
+public class DeleteOrganization extends BaseClass {
+	
+	@Test
+	public void DeleteOrg() {
+		System.out.println("Current Organization is deleted");
+	}
+
+}
